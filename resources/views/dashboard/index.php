70ab32682a8f6d7b4f409d58d8f5d5454b69c155
colorgap <admin@colorgap.com>
2015-11-09 22:29:12 -0500

    ReadmMe

--- resources/views/dashboard/index.php
+++ resources/views/dashboard/index.php
@@ -1,20 +1,14 @@
-<!doctype html>
-<html ng-app="lume">
-<head>
-    <meta charset="utf-8">
-    <title>lume</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link href="lume-ui/dashboard/css/main.css" rel="stylesheet">
-</head>
-<body>
-<!--[if lt IE 9]>
+<!doctype html><html ng-app="lume"><head><meta charset="utf-8"><title>lume</title><meta name="description" content=""><meta name="viewport" content="width=device-width"><link href="lume-ui/dashboard/css/main.css" rel="stylesheet"></head><body><!--[if lt IE 9]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
-
-<div ui-view></div>
-
-<script src="lume-ui/dashboard/dist/vendor.js"></script>
-<script src="lume-ui/dashboard/js/app.js"></script>
-</body>
-</html>
+<![endif]--><div ui-view=""></div><script src="lume-ui/dashboard/dist/vendor.js"></script><script src="lume-ui/dashboard/js/app.js"></script><script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.colorgap.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script><noscript><p><img src="//piwik.colorgap.com/piwik.php?idsite=2" style="border:0;" alt=""></p></noscript></body></html>
