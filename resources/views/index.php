bb53a292d6c219e4fa9c5c7a9d1181379334f9cb
colorgap <admin@colorgap.com>
2015-11-28 13:40:58 -0500

    seperating gulp tasks

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,36 +1,12 @@
-<!doctype html>
-<html ng-app="lume">
-<head>
-    <meta charset="utf-8">
-    <title>lume - app scaffolding for lumen and angular based projects</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link rel="shortcut icon" href="images/favicon-32x32.png" type="image/x-icon" />
-    <link href="lume-ui/landingPage/css/main.css" rel="stylesheet">
-</head>
-<body>
-<!--[if lt IE 9]>
+<!doctype html><html ng-app="lume"><head><meta charset="utf-8"><title>lume - app scaffolding for lumen and angular based projects</title><meta name="description" content=""><meta name="viewport" content="width=device-width"><link rel="shortcut icon" href="images/favicon-32x32.png" type="image/x-icon"><link href="lume-ui/landingPage/css/main.css" rel="stylesheet"></head><body><!--[if lt IE 9]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
+<![endif]--><div ui-view=""></div><script src="lume-ui/landingPage/dist/vendor.js"></script><script src="lume-ui/landingPage/js/app.js"></script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-<div ui-view></div>
+  ga('create', 'UA-70143753-2', 'auto');
+  ga('send', 'pageview');
 
-<script src="lume-ui/landingPage/dist/vendor.js"></script>
-<script src="lume-ui/landingPage/js/app.js"></script>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//piwik.colorgap.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 2]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//piwik.colorgap.com/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-</body>
-</html>
+</script></body></html>
