1530670794d77e4c7f622500c7d2d939793c2744
colorgap <admin@colorgap.com>
2015-11-28 23:18:27 -0500

    gulp build process has been added

--- gulp-tasks/build/build-app.js
+++ gulp-tasks/build/build-app.js
@@ -11,27 +11,7 @@ module.exports = function(gulp, plugins,config,env){
         './.env',
         './server.php'
     ];
-      // the base option sets the relative root for the set of files,
-      // preserving the folder structure
-      return gulp.src(filesToMove, { base: './' })
+    return gulp.src(filesToMove, { base: './' })
       .pipe(gulp.dest('tmp'));
-    // gulp.src(['./app/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/app'));
-    // gulp.src(['./bootstrap/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/bootstrap'));
-    // gulp.src(['./database/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/database'));
-    // gulp.src(['./resources/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/resources'));
-    // gulp.src(['./storage/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/storage'));
-    // gulp.src(['./vendor/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/vendor'));
-    // gulp.src(['./public/**/**/*.*'])
-    //   .pipe(gulp.dest('./tmp/public'));
-    // gulp.src(['./.env'])
-    //   .pipe(gulp.dest('./tmp/'));
-    // gulp.src(['./server.php'])
-    //   .pipe(gulp.dest('./tmp/'));
   };
 };
