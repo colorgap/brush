6cf173af59eb41d73f36a8f2808c564c013946ed
colorgap <admin@colorgap.com>
2015-11-28 23:17:22 -0500

    gulp build process has been added

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,22 +1,6 @@
-<!doctype html>
-<html ng-app="lume">
-<head>
-    <meta charset="utf-8">
-    <title>lume - app scaffolding for lumen and angular based projects</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link rel="shortcut icon" href="images/favicon-32x32.png" type="image/x-icon" />
-    <link href="lume-ui/landingPage/css/main.css" rel="stylesheet">
-</head>
-<body>
-<!--[if lt IE 9]>
+<!doctype html><html ng-app="lume"><head><meta charset="utf-8"><title>lume - app scaffolding for lumen and angular based projects</title><meta name="description" content=""><meta name="viewport" content="width=device-width"><link rel="shortcut icon" href="images/favicon-32x32.png" type="image/x-icon"><link href="lume-ui/landingPage/css/main.css" rel="stylesheet"></head><body><!--[if lt IE 9]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
-
-<div ui-view></div>
-<script src="lume-ui/landingPage/dist/vendor.js"></script>
-<script src="lume-ui/landingPage/js/app.js"></script>
-<script>
+<![endif]--><div ui-view=""></div><script src="lume-ui/landingPage/dist/vendor.js"></script><script src="lume-ui/landingPage/js/app.js"></script><script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -25,6 +9,4 @@
   ga('create', 'UA-70143753-2', 'auto');
   ga('send', 'pageview');
 
-</script>
-</body>
-</html>
+</script></body></html>
