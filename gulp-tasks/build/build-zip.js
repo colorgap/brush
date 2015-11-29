93a43fa6fb174c1e42c61f6132dbc23be853c433
colorgap <admin@colorgap.com>
2015-11-29 15:41:51 -0500

    gulp build process has been added

--- gulp-tasks/build/build-zip.js
+++ gulp-tasks/build/build-zip.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp, plugins,config,env){
   var zip = require('gulp-zip');
   return function(){
-      return gulp.src('./tmp/**')
+      return gulp.src('./tmp/**',{dot:true})
           .pipe(zip('lume-app.zip'))
           .pipe(gulp.dest('target'));
   };
