5494e8d6a2fb695b1602a7a2fd95fa20c2bddfa0
colorgap <admin@colorgap.com>
2015-12-06 11:47:47 -0500

    register pages

--- lume-ui/dashboard/app/app.route.js
+++ lume-ui/dashboard/app/app.route.js
@@ -13,6 +13,10 @@
         url: '/login',
         templateUrl: 'lume-ui/dashboard/partials/login/login.html'
       })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'lume-ui/dashboard/partials/register/register.html'
+      })
       .state('dashboard', {
         url: '/',
         templateUrl: 'lume-ui/dashboard/partials/dashboard/dashboard.html'
