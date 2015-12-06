0a42ca1c28b0582b488e73e09a9dd301286c58f9
colorgap <admin@colorgap.com>
2015-12-06 14:40:38 -0500

    adding simple admin pages

--- app/Models/Admin/Users.php
+++ app/Models/Admin/Users.php
@@ -20,5 +20,5 @@ class Users extends Model{
      *
      * @var array
      */
-    protected $hidden = ['password','remember_token','email','user_id', 'created_at', 'updated_at'];
+    protected $hidden = ['password','remember_token'];
 }
