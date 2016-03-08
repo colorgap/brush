6ff79d2d4298b2a35058e7f5032758eaedd9410d
colorgap <admin@colorgap.com>
2016-03-07 19:25:51 -0500

    material file changes

--- themes/bowyer-material/landingPage/app/app.route.js
+++ themes/bowyer-material/landingPage/app/app.route.js
@@ -11,7 +11,7 @@
       .state('home', {
         url: '/',
         templateUrl: 'bowyer-ui/landingPage/partials/home/index.html',
-        controller: 'homeCtrl'
+        controller: 'homeCtrl as hc'
       });
 
     $urlRouterProvider.otherwise('/');
