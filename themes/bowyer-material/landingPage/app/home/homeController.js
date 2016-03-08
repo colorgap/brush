7555250f8ee633abd550db7379d3dbd3828965eb
colorgap <admin@colorgap.com>
2016-03-07 19:25:51 -0500

    material file changes

--- themes/bowyer-material/landingPage/app/home/homeController.js
+++ themes/bowyer-material/landingPage/app/home/homeController.js
@@ -2,11 +2,6 @@
   'use strict';
   angular
     .module('bowyer').controller('homeCtrl',['$scope','$window', function($scope,$window){
-      $scope.scrollToSection = function(section){
-        angular.element('html, body').stop().animate({
-            scrollTop: angular.element('#'+section).offset().top
-        }, 1000);
-        
-      };
+  
   }]);
 })();
