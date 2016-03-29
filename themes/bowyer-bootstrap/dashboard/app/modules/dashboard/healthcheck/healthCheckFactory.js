a6cbd21a3ec2b7331ae20e61023bd30b9b2ea9d4
colorgap <admin@colorgap.com>
2016-03-29 19:54:15 -0400

    Healthcheck code improvements

---
+++ themes/bowyer-bootstrap/dashboard/app/modules/dashboard/healthcheck/healthCheckFactory.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+    angular.module('bowyer').factory('healthCheckFactory', function() {
+        return {
+            initial: {
+                checkValue: 0,
+                checkMessage: 'checking ...',
+                type: 'warning'
+            },
+            success: {
+                checkValue: 100,
+                checkMessage: 'Healthy',
+                checkPass: true,
+                type: 'success'
+            },
+            failed: {
+                checkValue: 100,
+                checkMessage: 'Failed',
+                type: 'danger'
+            }
+        };
+    });
+})();
