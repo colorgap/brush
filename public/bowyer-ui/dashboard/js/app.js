a9ad2049b654897924f11a0eaa60d616a056551a
colorgap <admin@colorgap.com>
2016-04-10 20:13:24 -0400

    fix for minification

--- public/bowyer-ui/dashboard/js/app.js
+++ public/bowyer-ui/dashboard/js/app.js
@@ -15,100 +15,101 @@ var bowyerApp;
 })();
 (function() {
     'use strict';
+    var routeConfig = ['$stateProvider', '$urlRouterProvider', 'localStorageServiceProvider',
+        function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
+            $stateProvider
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'bowyer-ui/dashboard/partials/login/login.html',
+                    controller: 'loginCtrl',
+                    controllerAs: 'login'
+                })
+                .state('logout', {
+                    url: '/logout',
+                    controller: 'logoutCtrl',
+                    controllerAs: 'logout'
+                })
+                .state('forgotPassword', {
+                    url: '/forgotPassword',
+                    templateUrl: 'bowyer-ui/dashboard/partials/forgotPassword/forgotPassword.html'
+                })
+                .state('register', {
+                    url: '/register',
+                    templateUrl: 'bowyer-ui/dashboard/partials/register/register.html',
+                    controller: 'registerCtrl',
+                    controllerAs: 'register'
+                })
+                .state('dashboard', {
+                    url: '/',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/dashboard.html'
+                })
+                .state('dashboard.alerts', {
+                    url: 'alerts',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/alerts/alerts.html'
+                })
+                .state('dashboard.users', {
+                    url: 'users',
+                    controller: 'usersCtrl',
+                    controllerAs: 'users',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/users/users.html'
+                })
+                .state('dashboard.profile', {
+                    url: 'profile',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/profile/profile.html',
+                    controller: 'profileCtrl',
+                    controllerAs: 'profile'
+                })
+                .state('dashboard.resetPassword', {
+                    url: 'resetPassword',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/resetPassword/resetPassword.html',
+                    controller: 'resetPasswordCtrl',
+                    controllerAs: 'resetPassword'
+                })
+                .state('dashboard.api', {
+                    url: 'api',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/api/api.html'
+                })
+                .state('dashboard.analytics', {
+                    url: 'analytics',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/analytics/analytics.html'
+                })
+                .state('dashboard.config', {
+                    url: 'config',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/config.html',
+                    controller: 'configCtrl',
+                    controllerAs: 'config'
+                })
+                .state('dashboard.config.roles', {
+                    url: '/roles',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/roles/roles.html',
+                    controller: 'rolesCtrl',
+                    controllerAs: 'roles'
+                })
+                .state('dashboard.config.navigation', {
+                    url: '/navigation',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/navigation/navigation.html'
+                })
+                .state('dashboard.healthcheck', {
+                    url: 'healthcheck',
+                    templateUrl: 'bowyer-ui/dashboard/partials/dashboard/healthcheck/healthcheck.html',
+                    controller: 'healthCheckCtrl as vm'
+                });
+            $urlRouterProvider.otherwise('/login');
+            localStorageServiceProvider.setPrefix('bowyer');
+        }];
     bowyerApp.config(routeConfig);
-    function routeConfig($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
-        $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: 'bowyer-ui/dashboard/partials/login/login.html',
-                controller: 'loginCtrl',
-                controllerAs: 'login'
-            })
-            .state('logout', {
-                url: '/logout',
-                controller: 'logoutCtrl',
-                controllerAs: 'logout'
-            })
-            .state('forgotPassword', {
-                url: '/forgotPassword',
-                templateUrl: 'bowyer-ui/dashboard/partials/forgotPassword/forgotPassword.html'
-            })
-            .state('register', {
-                url: '/register',
-                templateUrl: 'bowyer-ui/dashboard/partials/register/register.html',
-                controller: 'registerCtrl',
-                controllerAs: 'register'
-            })
-            .state('dashboard', {
-                url: '/',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/dashboard.html'
-            })
-            .state('dashboard.alerts', {
-                url: 'alerts',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/alerts/alerts.html'
-            })
-            .state('dashboard.users', {
-                url: 'users',
-                controller: 'usersCtrl',
-                controllerAs: 'users',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/users/users.html'
-            })
-            .state('dashboard.profile', {
-                url: 'profile',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/profile/profile.html',
-                controller: 'profileCtrl',
-                controllerAs: 'profile'
-            })
-            .state('dashboard.resetPassword', {
-                url: 'resetPassword',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/resetPassword/resetPassword.html',
-                controller: 'resetPasswordCtrl',
-                controllerAs: 'resetPassword'
-            })
-            .state('dashboard.api', {
-                url: 'api',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/api/api.html'
-            })
-            .state('dashboard.analytics', {
-                url: 'analytics',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/analytics/analytics.html'
-            })
-            .state('dashboard.config', {
-                url: 'config',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/config.html',
-                controller: 'configCtrl',
-                controllerAs: 'config'
-            })
-            .state('dashboard.config.roles', {
-                url: '/roles',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/roles/roles.html',
-                controller: 'rolesCtrl',
-                controllerAs: 'roles'
-            })
-            .state('dashboard.config.navigation', {
-                url: '/navigation',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/config/navigation/navigation.html'
-            })
-            .state('dashboard.healthcheck', {
-                url: 'healthcheck',
-                templateUrl: 'bowyer-ui/dashboard/partials/dashboard/healthcheck/healthcheck.html',
-                controller: 'healthCheckCtrl as vm'
-            });
-        $urlRouterProvider.otherwise('/login');
-        localStorageServiceProvider.setPrefix('bowyer');
-    }
 })();
 (function() {
     'use strict';
-    bowyerApp.run(runBlock);
-    function runBlock($http, localStorageService) {
+    var runBlock = ['$http', 'localStorageService',function ($http, localStorageService) {
         $http.defaults.headers.common.apitoken = localStorageService.get('api_token');
-    }
+    }];
+    bowyerApp.run(runBlock);
 })();
 
 (function() {
   'use strict';
-  angular.module('bowyer').controller('dashboardCtrl', ['$scope',function($scope){
+  bowyerApp.controller('dashboardCtrl', ['$scope',function($scope){
 
   }]);
 })();
--- themes/bowyer-bootstrap/dashboard/app/modules/dashboard/dashboardController.js
+++ themes/bowyer-bootstrap/dashboard/app/modules/dashboard/dashboardController.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  angular.module('bowyer').controller('dashboardCtrl', ['$scope',function($scope){
+  bowyerApp.controller('dashboardCtrl', ['$scope',function($scope){
 
   }]);
 })();
