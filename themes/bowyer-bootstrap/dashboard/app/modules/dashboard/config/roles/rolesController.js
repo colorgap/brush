8456521ca8b5aa17f8f791a4bd776bf76a5aa5f9
Color][gap <admin@colorgap.com>
2016-07-10 16:37:07 -0400

    code

--- themes/bowyer-bootstrap/dashboard/app/modules/dashboard/config/roles/rolesController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    bowyerApp.controller('rolesCtrl', ['api', 'commonFactory', function(api, commonFactory) {
-        var roles = this;
-        commonFactory.getAdminRoles(function(response){
-            roles.roles = response.data;
-        });
-    }]);
-})();
--- themes/bowyer-material/landingPage/app/home/homeController.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-  angular
-    .module('bowyer').controller('homeCtrl',['$scope','$window', function($scope,$window){
-  
-  }]);
-})();
