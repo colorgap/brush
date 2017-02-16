cc21d1e82a1baec6a81e9e9e9ad2a0a6a29dd28b
Color][gap <admin@colorgap.com>
2017-02-15 21:26:55 -0500

    Validation for reset pasword

--- themes/bowyer-bootstrap/dashboard/app/modules/resetPassword/resetPasswordController.js
+++ themes/bowyer-bootstrap/dashboard/app/modules/resetPassword/resetPasswordController.js
@@ -2,26 +2,31 @@
  * Author: Color][gap
  * Description: Forgot Password controller
  */
-(function() {
+(function () {
     'use strict';
-    bowyerApp.controller('resetForgotPasswordCtrl', ['$state','api','url','constants',
-        function($state,api, url, constants) {
+    bowyerApp.controller('resetForgotPasswordCtrl', ['$state', 'api', 'url', 'constants',
+        function ($state, api, url, constants) {
             var resetForgotPassword = this;
-            resetForgotPassword.validateLinkAndUpdatePassword = function() {
-                var resetForgotPasswordCallConfig = {
-                    url: url.resetForgotPassword,
-                    data: {
-                        token: $state.params.token,
-                        password: resetForgotPassword.password
-                    },
-                    method: constants.method.post
-                };
-                api.executeCall(resetForgotPasswordCallConfig, function(response){
-                    resetForgotPassword.resetForgotPasswordError = response.data;
-                    resetForgotPassword.username = '';
-                }, function(err){
-                    resetForgotPassword.error = err.data;
-                })
+            resetForgotPassword.validateLinkAndUpdatePassword = function () {
+                if (resetForgotPassword.password === resetForgotPassword.confirmpassword) {
+                    var resetForgotPasswordCallConfig = {
+                        url: url.resetForgotPassword,
+                        data: {
+                            token: $state.params.token,
+                            password: resetForgotPassword.password
+                        },
+                        method: constants.method.post
+                    };
+                    api.executeCall(resetForgotPasswordCallConfig, function (response) {
+                        resetForgotPassword.resetForgotPasswordError = response.data;
+                        resetForgotPassword.confirmpassword = '';
+                        resetForgotPassword.password === '';
+                    }, function (err) {
+                        resetForgotPassword.error = err.data;
+                    });
+                }else{
+                    resetForgotPassword.error = {confirmpassword : ["Password doesn't match Confirm Password."]};
+                }
             };
         }]);
 })();
